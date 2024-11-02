3a14a7bbf5ca440604f1e44667d9bc18ed6ae772
dongugchoi <163122705+dongugchoi@users.noreply.github.com>
2024-11-02 12:44:43 +0900

    2024-11-02

--- Work/blog-app/src/page/Login.jsx
+++ Work/blog-app/src/page/Login.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <p>로그인 페이지 입니다.</p>
+      <p>ID</p>
+      <input type="text" placeholder='아이디를 입력하세요.'/>
+      <p>Password</p>
+      <input type="password" placeholder='비밀번호를 입력하세요.'/>
+      <div>
+        <button>로그인</button>
+      </div>
+      
     </div>
+    
   )
 }
 
