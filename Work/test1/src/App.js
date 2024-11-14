8a42df5e9d3f25fe76947ee073cc1cf9af160deb
dongugchoi <163122705+dongugchoi@users.noreply.github.com>
2024-11-14 21:58:21 +0900

    2024-11-14 React Hook Study
    2024-11-14 React Hook Study

--- Work/test1/src/App.js
+++ Work/test1/src/App.js
@@ -7,19 +7,14 @@ import Box3 from './components/Box3';
 import Welcome2 from './components/Welcome2';
 import Wrapbox from './components/Wrapbox';
 import Box4 from './components/Box4';
+import Count from './components/Count';
 
 
 function App() {
   return (
 
     <Wrapbox >
-       <Box2 name="빨강박스" color='red'/>
-       <Box2 name="노랑박스" color='yellow'/>
-       <Box2 name="파랑박스" color='blue'/>
-       <Box4 name="파랑박스" color='blue'/>
-       <li>aaa</li>
-       <li>bbb</li>
-       <li>ccc</li>
+      <Count />
     </Wrapbox>
 
   );
