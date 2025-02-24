f74e55373f510a2a44859dabf3f61d23faf3c078
dongugchoi <ehddnr7321@gmail.com>
2025-02-24 20:41:09 +0900

    Components Study

---
+++ Vue/npm-vue/ch05/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FirstChild from "./components/FirstChild.vue";
+
+
+</script>
+
+<template>
+  <FirstChild/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Vue/npm-vue/ch05/src/components/FirstChild.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>컴포넌트</h1>
+    <p>내가 만든 첫 번째 컴포넌트</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    h1{
+        color: blue;
+    }
+
+    p{
+        color: red;
+    }
+</style>
+
