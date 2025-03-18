3130746e508f413f3c767b75a68763a5fde2b000
dongugchoi <ehddnr7321@gmail.com>
2025-03-18 22:04:02 +0900

    flex-scroll-test

--- Vue/flexTest/src/App.vue
+++ Vue/flexTest/src/App.vue
@@ -1,56 +1,18 @@
 <template>
-  <div class="container">
-    <div class="box red">박스 1</div>
-    <div class="box blue">박스 2</div>
-    <div class="box green">박스 3</div>
-    <div class="box purple">박스 4</div>
-  </div>
+    <flexTest />
+    <flexScroll />
 </template>
 
 <script>
+import flexTest from './components/flexTest.vue';
+import flexScroll from './components/flexScroll.vue';
+
 export default {
-  name: "App",
+  components: { flexTest, flexScroll },
+ 
 };
 </script>
 
 <style scoped>
 
-.container {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  border: 2px solid black;
-}
-
-
-.box {
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 20px;
-  font-weight: bold;
-  color: white;
-  border-bottom: 2px solid black;
-}
-
-.box:last-child {
-  border-bottom: none;
-}
-
-.red {
-  background-color: red;
-}
-
-.blue {
-  background-color: blue;
-}
-
-.green {
-  background-color: green;
-}
-
-.purple {
-  background-color: purple;
-}
 </style>
---
+++ Vue/flexTest/src/components/flexScroll.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+      <div v-for="(item, index) in items" :key="index" class="item">
+        {{ item }}
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        items: this.generateItems()
+      };
+    },
+    methods: {
+      generateItems() {
+        let items = [];
+        for (let i = 1; i <= 30; i++) {
+          items.push(`Item ${i}`);
+        }
+        return items;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    width: 300px;
+    height: 200px;
+    border: 1px solid #ccc;
+    padding: 10px;
+    overflow-y: auto;
+  }
+  
+  .item {
+    flex: 0 0 30%;
+    background: lightblue;
+    padding: 10px;
+    text-align: center;
+    border-radius: 5px;
+  }
+  </style>
