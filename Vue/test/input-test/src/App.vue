1a2e3421e5b250f3665e0b5f8686fe747ff78699
dongugchoi <ehddnr7321@gmail.com>
2025-04-17 20:09:06 +0900

    인풋 한글테스트

---
+++ Vue/test/input-test/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <input
+      type="text"
+      :value="message"
+      @input="message = $event.target.value"
+    />
+    <p>입력된 메시지: {{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: ''
+    };
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  padding: 2rem;
+}
+input {
+  padding: 0.5rem;
+  font-size: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
