a6b82de1f4142b7956d8a89f817f0d4edcc30427
dongugchoi <ehddnr7321@gmail.com>
2025-05-08 20:26:51 +0900

    asign-test

---
+++ Vue/asign-test/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>이름: {{ user.name }}</p>
+    <p>나이: {{ user.age }}</p>
+    <p v-if="user.city">도시 :{{user.city }}</p>
+    <button @click="updateUser">유저 정보 업데이트</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: '홍길동',
+        age: 25
+      },
+      newUserData: {
+        age: 30,
+        city: 'Seoul'
+      }
+    };
+  },
+  methods: {
+    updateUser() {
+      // 기존 user 객체를 변경
+      Object.assign(this.user, this.newUserData);
+    }
+  }
+};
+</script>
