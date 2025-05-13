b1b1f47ccd499240652334ae6cecf16cb30fa8d4
dongugchoi <ehddnr7321@gmail.com>
2025-05-13 20:18:05 +0900

    apply-test

---
+++ Vue/test/maxapply-test/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>숫자 배열: {{ numbers }}</h2>
+    <p>최댓값: {{ maxNumber }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: [10, 30, 5, 67, 22]
+    };
+  },
+  computed: {
+    maxNumber() {
+      return Math.max.apply(null, this.numbers);
+      // 또는: return Math.max(...this.numbers); (ES6 spread 연산자)
+    }
+  }
+};
+</script>
